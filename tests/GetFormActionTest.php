1e43942e13cab3f92bea0adf55bd86f455196f99
Takeshi <yamazakitakeshi02@gmail.com>
2020-12-28 22:01:07 +0900

    test: 記事表示機能のテストコードを追加

--- tests/GetFormActionTest.php
+++ tests/GetFormActionTest.php
@@ -8,8 +8,27 @@ require('model/GetFormAction.php');
 
 class GetFormActionTest extends TestCase
 {
+    protected static $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        try {
+            self::$pdo = new PDO(PDO_DSN, DATABASE_USER, DATABASE_PASSWORD);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            print('Error:'.$e->getMessage());
+            die();
+        }
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$pdo = null;
+    }
+
     /**
-     * @dataProvider postDataProvider
+     * @dataProvider saveDataProvider
      */
     public function testSaveDBPostData($data, $expected)
     {
@@ -24,21 +43,13 @@ class GetFormActionTest extends TestCase
         $this->assertEquals($expected, $actual_postResult);
         
         // 4. SQL文で直接記事を取得を試みる
-        try {
-            $pdo = new PDO(PDO_DSN, DATABASE_USER, DATABASE_PASSWORD);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        } catch (PDOException $e) {
-            print('Error:'.$e->getMessage());
-            die();
-        }
         if ($data['name'] == '') {
             $searchKey = $data['email'];
         } else {
             $searchKey = $data['name'];
         }
         $sql = "select * from posts where name = '$searchKey'";
-        $stmt = $pdo->query($sql);
+        $stmt = self::$pdo->query($sql);
         $actual_fetch = $stmt->fetch();
 
         // 5. アサーションメソッドで確認
@@ -54,12 +65,11 @@ class GetFormActionTest extends TestCase
         // 6. 後片付け
         if (is_array($actual_fetch) == true) {
             $sql = "delete from posts where name = '$data[name]'";
-            $stmt = $pdo->query($sql);
+            $stmt = self::$pdo->query($sql);
         }
-        $pdo = null;
     }
 
-    public function postDataProvider()
+    public function saveDataProvider()
     {
         return [
             'Successful' => [[
@@ -98,6 +108,50 @@ class GetFormActionTest extends TestCase
                 'post_body' => 'これはテストです',
                 'password' => 'password'
             ],false]
+        ];
+    }
+
+
+    /**
+     * @dataProvider getDataProvider
+     */
+    public function testGetDBPostData($data)
+    {
+        // 1. GetFormActionインスタンスを生成
+        $action = new GetFormAction();
+
+        // 2. SQL文で直接記事を登録
+        $smt = self::$pdo->prepare('insert into posts (name,email,body,password,posted_at,updated_at) values(:name,:email,:body,:password,now(),now())');
+        $smt->bindParam(':name', $data['name'], PDO::PARAM_STR);
+        $smt->bindParam(':email', $data['email'], PDO::PARAM_STR);
+        $smt->bindParam(':body', $data['post_body'], PDO::PARAM_STR);
+        $smt->bindParam(':password', $data['password'], PDO::PARAM_STR);
+        $smt->execute();
+
+        // 3. GetDBPostDataで記事データ取得
+        $actual_result = $action->GetDBPostData();
+        $testPost = $actual_result[0];
+        
+        // 4. それぞれ確認
+        $this->assertEquals($data['name'], $testPost['name']);
+        $this->assertEquals($data['email'], $testPost['email']);
+        $this->assertEquals($data['post_body'], $testPost['body']);
+        $this->assertEquals($data['password'], $testPost['password']);
+
+        // 5. 後片付け
+        $sql = "delete from posts where name = '$data[name]'";
+        $smt = self::$pdo->query($sql);
+    }
+
+    public function getDataProvider()
+    {
+        return [
+            'successful' => [[
+                'name' => 'gettest',
+                'email' => 'get@get.get',
+                'post_body' => 'gettest',
+                'password' => 'getget'
+            ]]
         ];
     }
 }

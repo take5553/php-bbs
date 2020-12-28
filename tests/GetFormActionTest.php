32cf5c71bf30810f2d11c7eccf2229af408ab3e3
Takeshi <yamazakitakeshi02@gmail.com>
2020-12-28 11:38:39 +0900

    refactor: 投稿機能のテストコードを整理

--- tests/GetFormActionTest.php
+++ tests/GetFormActionTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use phpDocumentor\Reflection\Types\Boolean;
 use PHPUnit\Framework\TestCase;
 
 require('config/properties_for_test.php');
@@ -7,116 +8,20 @@ require('model/GetFormAction.php');
 
 class GetFormActionTest extends TestCase
 {
-    public function testSaveDBPostDataSuccessful()
-    {
-        // 1. テスト記事データ作成
-        $action = new GetFormAction();
-        $testpost = array(
-            'name' => 'testpost',
-            'email' => 'hoge@hoge.hoge',
-            'post_body' => 'これはテストです',
-            'password' => 'password'
-        );
-
-        // 2. 投稿
-        $action->SaveDBPostData($testpost);
-        
-        // 3. SQL文で直接記事を取得
-        try {
-            $pdo = new PDO(PDO_DSN, DATABASE_USER, DATABASE_PASSWORD);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        } catch (PDOException $e) {
-            print('Error:'.$e->getMessage());
-            die();
-        }
-        $sql = "select * from posts where name = '$testpost[name]'";
-        $stmt = $pdo->query($sql);
-        $result = $stmt->fetch();
-
-        // 4. アサーションメソッドで確認
-        $this->assertEquals($testpost['name'], $result['name']);
-        $this->assertEquals($testpost['email'], $result['email']);
-        $this->assertEquals($testpost['post_body'], $result['body']);
-        $this->assertEquals($testpost['password'], $result['password']);
-
-        // 5. 今保存した記事を削除
-        $sql = "delete from posts where name = '$testpost[name]'";
-        $stmt = $pdo->query($sql);
-        $pdo = null;
-    }
-
-    public function testSaveDBPostDataWithoutName()
-    {
-        $testpost = array(
-            'name' => '',
-            'email' => 'hoge@hoge.hoge',
-            'post_body' => 'これはテストです',
-            'password' => 'password'
-        );
-
-        $this->failSaveDBPostData($testpost);
-    }
-
-    public function testSaveDBPostDataWithoutEmail()
-    {
-        $testpost = array(
-            'name' => 'testpost',
-            'email' => '',
-            'post_body' => 'これはテストです',
-            'password' => 'password'
-        );
-
-        $this->failSaveDBPostData($testpost);
-    }
-
-    public function testSaveDBPostDataWithoutPostBody()
-    {
-        $testpost = array(
-            'name' => 'testpost',
-            'email' => 'hoge@hoge.hoge',
-            'post_body' => '',
-            'password' => 'password'
-        );
-
-        $this->failSaveDBPostData($testpost);
-    }
-
-    public function testSaveDBPostDataWithoutPassword()
-    {
-        $testpost = array(
-            'name' => 'testpost',
-            'email' => 'hoge@hoge.hoge',
-            'post_body' => 'これはテストです',
-            'password' => ''
-        );
-
-        $this->failSaveDBPostData($testpost);
-    }
-
-    public function testSaveDBPostDataWithLongName()
-    {
-        $testpost = array(
-            'name' => '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
-            'email' => 'hoge@hoge.hoge',
-            'post_body' => 'これはテストです',
-            'password' => 'password'
-        );
-
-        $this->failSaveDBPostData($testpost);
-    }
-
-    private function failSaveDBPostData($data)
+    /**
+     * @dataProvider postDataProvider
+     */
+    public function testSaveDBPostData($data, $expected)
     {
 
-        // 1. テスト記事データ作成
+        // 1. GetFormActionインスタンスを生成
         $action = new GetFormAction();
 
         // 2. 投稿
-        $postResult = $action->SaveDBPostData($data);
+        $actual_postResult = $action->SaveDBPostData($data);
 
         // 3. アサーションメソッドで確認
-        $this->assertEquals(false, $postResult);
+        $this->assertEquals($expected, $actual_postResult);
         
         // 4. SQL文で直接記事を取得を試みる
         try {
@@ -134,12 +39,65 @@ class GetFormActionTest extends TestCase
         }
         $sql = "select * from posts where name = '$searchKey'";
         $stmt = $pdo->query($sql);
-        $result = $stmt->fetch();
+        $actual_fetch = $stmt->fetch();
 
         // 5. アサーションメソッドで確認
-        $this->assertEquals(false, $result);
+        $this->assertEquals($expected, is_array($actual_fetch));
+        if (is_array($actual_fetch) == true) {
+            // $actual_fetchが配列なら記事が取得できているはず
+            $this->assertEquals($data['name'], $actual_fetch['name']);
+            $this->assertEquals($data['email'], $actual_fetch['email']);
+            $this->assertEquals($data['post_body'], $actual_fetch['body']);
+            $this->assertEquals($data['password'], $actual_fetch['password']);
+        }
         
         // 6. 後片付け
+        if (is_array($actual_fetch) == true) {
+            $sql = "delete from posts where name = '$data[name]'";
+            $stmt = $pdo->query($sql);
+        }
         $pdo = null;
+    }
+
+    public function postDataProvider()
+    {
+        return [
+            'Successful' => [[
+                'name' => 'testpost',
+                'email' => 'hoge@hoge.hoge',
+                'post_body' => 'これはテストです',
+                'password' => 'password'
+            ],true],
+            'withoutName' => [[
+                'name' => '',
+                'email' => 'hoge@hoge.hoge',
+                'post_body' => 'これはテストです',
+                'password' => 'password'
+            ],false],
+            'withoutEmail' => [[
+                'name' => 'testpost',
+                'email' => '',
+                'post_body' => 'これはテストです',
+                'password' => 'password'
+            ],false],
+            'withoutBody' => [[
+                'name' => 'testpost',
+                'email' => 'hoge@hoge.hoge',
+                'post_body' => '',
+                'password' => 'password'
+            ],false],
+            'withoutPassword' => [[
+                'name' => 'testpost',
+                'email' => 'hoge@hoge.hoge',
+                'post_body' => 'これはテストです',
+                'password' => ''
+            ],false],
+            'withLongName' => [[
+                'name' => '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+                'email' => 'hoge@hoge.hoge',
+                'post_body' => 'これはテストです',
+                'password' => 'password'
+            ],false]
+        ];
     }
 }

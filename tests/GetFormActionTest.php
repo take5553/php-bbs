bac7be3ebab625843cf16bb15e4b4e8fda6255e9
Takeshi <yamazakitakeshi02@gmail.com>
2021-01-15 22:34:08 +0900

    test: 記事削除機能のテストを追加

--- tests/GetFormActionTest.php
+++ tests/GetFormActionTest.php
@@ -306,8 +306,54 @@ class GetFormActionTest extends TestCase
         $this->assertNotEquals($wrongData['email'], $actual_fetch['email']);
         $this->assertNotEquals($wrongData['body'], $actual_fetch['body']);
 
-        // 8. 後片付け
-        $sql = "delete from posts where id = $wrongData[id]";
+        // 8. testDeleteDBPostDataへ引き継ぐ
+        return $data;
+    }
+
+    /**
+     * @depends testUpdateDBPostData
+     */
+    public function testDeleteDBPostData($originalPostData)
+    {
+        // 1. GetFormActionインスタンスを生成
+        $action = new GetFormAction();
+
+        // 2. パスワードが違うと削除できない
+        $result = $action->DeleteDBPostData((int)$originalPostData['id'], "hugahuga");
+        $this->assertFalse($result);
+
+        // 3. SQL文で直接記事を取得を試みる
+        $sql = "select * from posts where id = $originalPostData[id]";
+        $stmt = self::$pdo->query($sql);
+        $actual_fetch = $stmt->fetch();
+
+        // 4. 論理削除されていないことを確認
+        $this->assertNull($actual_fetch['deleted_at']);
+
+        // 5. パスワードが正しいと削除できる
+        $result = $action->DeleteDBPostData((int)$originalPostData['id'], $originalPostData['password']);
+        $this->assertTrue($result);
+
+        // 6. SQL文で直接記事を取得を試みる
+        $sql = "select * from posts where id = $originalPostData[id]";
+        $stmt = self::$pdo->query($sql);
+        $actual_fetch = $stmt->fetch();
+
+        // 7. 論理削除されていることを確認
+        $this->assertNotNull($actual_fetch['deleted_at']);
+
+        // 8. GetDBPostDataで記事が取得できないことを確認
+        $actual_results = $action->GetDBPostData();
+        $postDatafound = false;
+        foreach ($actual_results as $actual_result) {
+            if ($actual_result['id'] == $originalPostData['id']) {
+                $postDatafound = true;
+            }
+        }
+        $this->assertFalse($postDatafound);
+
+        // 9. 後片付け
+        $sql = "delete from posts where id = $originalPostData[id]";
         $smt = self::$pdo->query($sql);
     }
 }
